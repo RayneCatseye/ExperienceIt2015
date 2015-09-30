1ecbd70146d3e73c3713e0812816422480b6e3b3
Emily Saunders Walmsley <EmSaundWalm@gmail.com>
2015-09-30 23:41:33 +1000

    Initial commit

---
+++ sentiment.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+romantic_words = ['love', 'sigh', 'happy', 'gaze', 'cherish', 'enchanting', 
+                  'everlasting', 'gorgeous', 'handsome', 'wonderful']
+
+hp = open('HarryPotter.txt')
+hp_word_counter = 0
+hp_romance_counter = 0
+
+for line in hp:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+        hp_word_counter = hp_word_counter + 1
+        if word in romantic_words:
+            hp_romance_counter = hp_romance_counter + 1
+
+#print(str(hp_romance_counter) + '/' + str(hp_word_counter))
+
+tw = open('Twilight.txt')
+tw_word_counter = 0
+tw_romance_counter = 0
+
+for line in tw:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+        tw_word_counter = tw_word_counter + 1
+        if word in romantic_words:
+            tw_romance_counter = tw_romance_counter + 1
+
+#print(str(tw_romance_counter) + '/' + str(tw_word_counter))
+
+if (tw_romance_counter/float(tw_word_counter)) > (hp_romance_counter/float(hp_word_counter)):
+    print ('Twilight is more romantic with ' + str(tw_romance_counter) + ' romantic words.')
+else:
+    print ('Harry Potter is more romantic.')
